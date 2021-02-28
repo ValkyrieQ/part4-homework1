cb70d2f50926cab24d0850e707e0f2a554b86142
Hikaruweng <hikaruweng@Hikaruwengs-MacBook-Pro.local>
2021-02-28 17:29:45 +0700

    Finish Part4 Homework1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'part4-homework1';
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      comments: [''],
+    });
+  }
+
+  submitForm() {
+    alert(JSON.stringify(this.form.value));
+  }
 }
